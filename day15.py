c88020301910598e310e20e4f72c74a995ffb1f2
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2021-12-15 21:17:01 +0100

    Use priority queue for open_set

--- day15.py
+++ day15.py
@@ -1,5 +1,7 @@
 import numpy as np
+import heapq
 from collections import defaultdict
+
 
 def parse_matrix(filename):
     lines = [line.rstrip() for line in open(filename).readlines()]
@@ -13,7 +15,7 @@ def manhatten_distance(pos_a, pos_b):
 def get_neighbors(pos, matrix_shape):
     rows, cols = matrix_shape
     result = []
-    for row in range(max(pos[0] - 1, 0), min(pos[0] + 2, rows)):
+    for row in range(max(pos[0]- 1, 0), min(pos[0] + 2, rows)):
         result.append((row, pos[1]))
     for col in range(max(pos[1] - 1, 0), min(pos[1] + 2, cols)):
         result.append((pos[0], col))
@@ -46,13 +48,13 @@ def a_star(matrix, start_pos, end_pos, factor):
     g_score[start_pos] = 0
     f_score[end_pos] = manhatten_distance(start_pos, end_pos)
     came_from = {}
-    open_set = [start_pos]
+    open_set = []
+    heapq.heappush(open_set, (f_score[start_pos], start_pos))
     rows, cols = matrix.shape
     matrix_shape = [rows * factor, cols * factor]
 
     while len(open_set):
-        open_set.sort(key=lambda pos: f_score[pos])
-        current = open_set.pop(0)
+        current_f_score, current = heapq.heappop(open_set)
         if current == end_pos:
             return calculate_risk(matrix, came_from, start_pos, end_pos)
 
@@ -62,9 +64,10 @@ def a_star(matrix, start_pos, end_pos, factor):
             if tentative_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = tentative_g_score + manhatten_distance(neighbor, end_pos)
+                neighbor_f_score = tentative_g_score + manhatten_distance(neighbor, end_pos)
+                f_score[neighbor] = neighbor_f_score
                 if neighbor not in open_set:
-                    open_set.append(neighbor)
+                    heapq.heappush(open_set, (neighbor_f_score, neighbor))
 
 
 def main():
